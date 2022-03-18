7df4337c3138c9c1f6f2ce347287d9419bb4e3d4
LettaVio <101905746+LettaVio@users.noreply.github.com>
2022-03-19 00:18:43 +0200

    файлы домашки 1
    файлы домашки 1

---
+++ js/tabs.js
@@ -0,0 +1,46 @@
+const tabButtons = document.querySelectorAll(".design-list__item");
+const tabDescriptios = document.querySelectorAll('.design__descr');
+const tabImages = document.querySelectorAll('.design-images')
+const tabLeftImgs = document.querySelectorAll('.design-block__img')
+const tabTitles = document.querySelectorAll('.design__title');
+
+
+
+
+const changeContent = (array, value) => {
+    array.forEach((elem) => {
+        if (elem.dataset.tabsField === value) {
+            elem.classList.remove('hidden')
+
+        } else {
+            elem.classList.add('hidden')
+        }
+    })
+}
+
+tabButtons.forEach((tabButton) => {
+    tabButton.addEventListener('click', (event) => {
+        const dataValue = tabButton.dataset.tabsHandler
+        changeContent(tabDescriptios, dataValue)
+        changeContent(tabImages, dataValue)
+        changeContent(tabLeftImgs, dataValue)
+        changeContent(tabTitles, dataValue)
+
+        tabTitles.forEach((title) => {
+            if (!title.classList.contains("hidden")) {
+                document.title = title.innerText;
+            }
+        })
+
+
+
+        tabButtons.forEach((btn) => {
+            if (btn === event.target) {
+                btn.classList.add('design-list__item_active')
+            } else {
+                btn.classList.remove('design-list__item_active')
+            }
+        })
+
+    })
+})
