768b155a23ede24fda0b80145e4c051c458ccadc
LettaVio <viollogika@gmail.com>
2022-03-21 15:03:38 +0200

    исправила ошибки день 3

--- js/timer.js
+++ js/timer.js
@@ -2,6 +2,23 @@ const timerBlock = document.querySelector('.timer__time')
 const daedLine = '31 march 2022'
 let interval
 
+const getNoun = (number, one, two, five) => {
+    let n = Math.abs(number);
+    n %= 100;
+    if (n >= 5 && n <= 20) {
+        return five;
+    }
+    n %= 10;
+    if (n === 1) {
+        return one;
+    }
+    if (n >= 2 && n <= 4) {
+        return two;
+    }
+    return five;
+}
+
+
 const updateClock = () => {
     const date = new Date().getTime()
     const dateDeadLine = new Date(daedLine).getTime()
@@ -12,16 +29,22 @@ const updateClock = () => {
     const minutes = Math.floor((timeRemaining / 60) % 60)
     const sec = Math.floor(timeRemaining % 60)
 
+
+
     const fDays = days < 10 ? '0' + days : days
     const fHours = hours < 10 ? '0' + hours : hours
     const fMinutes = minutes < 10 ? '0' + minutes : minutes
     const fSec = sec < 10 ? '0' + sec : sec
 
-    timerBlock.textContent = `${fDays }:${fHours }:${fMinutes}:${fSec}`
+
     if (timeRemaining <= 0) {
         clearInterval(interval)
         timerBlock.textContent = "00:00:00:00"
+    } else {
+        timerBlock.textContent = `${fDays + getNoun(fDays, " день ", " дня ", " дней ") } ${fHours }:${fMinutes}:${fSec}`
+
     }
+
 
 }
 
